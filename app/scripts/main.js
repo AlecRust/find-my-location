a26fc1bd457a0b684b2645f031e3890545805c22
Alec Rust <me@alecrust.com>
2014-04-21 18:59:04 +0100

    Initial commit yo

---
+++ app/scripts/main.js
@@ -0,0 +1,58 @@
+/*!
+ * Basic usage of the HTML5 Geolocation API to plot users' location on a map
+ */
+
+'use strict';
+
+var map,
+    google;
+
+function initialize() {
+    var mapOptions = {
+        zoom: 6
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+    // Try HTML5 geolocation
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new google.maps.LatLng(position.coords.latitude,
+                position.coords.longitude);
+
+            var infowindow = new google.maps.InfoWindow({
+                map: map,
+                position: pos,
+                content: 'Here you are!'
+            });
+
+            map.setCenter(pos);
+        }, function () {
+            handleNoGeolocation(true);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleNoGeolocation(false);
+    }
+}
+
+function handleNoGeolocation(errorFlag) {
+    var errorContent;
+    
+    if (errorFlag) {
+        errorContent = 'Error: The Geolocation service failed.';
+    } else {
+        errorContent = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60, 105),
+        content: errorContent
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
