72e57629a6463ea57737b0fd829504869374ab88
Alec Rust <me@alecrust.com>
2014-04-26 18:08:11 +0100

    Include apple-touch-icon in file revving

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,8 +176,7 @@ module.exports = function (grunt) {
                         '<%= config.dist %>/styles/{,*/}*.css',
                         '<%= config.dist %>/images/{,*/}*.*',
                         '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}',
-                        '!<%= config.dist %>/apple-touch-icon.png'
+                        '<%= config.dist %>/*.{ico,png}'
                     ]
                 }
             }
