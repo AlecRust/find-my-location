0ffa50437dfbe6f8384f632c2b20bdaa09d2dd58
Alec Rust <me@alecrust.com>
2014-04-27 21:51:35 +0100

    Switch from 4 to 2 tab spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,311 +2,311 @@
 
 module.exports = function (grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
 
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Configurable paths
-    var config = {
-        app: 'app',
-        dist: 'dist'
-    };
+  // Configurable paths
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
 
-    // Define the configuration for all the tasks
-    grunt.initConfig({
+  // Define the configuration for all the tasks
+  grunt.initConfig({
 
-        // Project settings
-        config: config,
+    // Project settings
+    config: config,
 
-        // Watches files for changes and runs tasks based on the changed files
-        watch: {
-            bower: {
-                files: ['bower.json'],
-                tasks: ['bowerInstall']
-            },
-            js: {
-                files: ['<%= config.app %>/scripts/{,*/}*.js'],
-                tasks: ['jshint'],
-                options: {
-                    livereload: true
-                }
-            },
-            gruntfile: {
-                files: ['Gruntfile.js']
-            },
-            sass: {
-                files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['sass:server', 'autoprefixer']
-            },
-            styles: {
-                files: ['<%= config.app %>/styles/{,*/}*.css'],
-                tasks: ['newer:copy:styles', 'autoprefixer']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= config.app %>/{,*/}*.html',
-                    '.tmp/styles/{,*/}*.css',
-                    '<%= config.app %>/images/{,*/}*'
-                ]
-            }
-        },
-
-        // The actual grunt server settings
-        connect: {
-            options: {
-                port: 9000,
-                open: true,
-                livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    middleware: function(connect) {
-                        return [
-                            connect.static('.tmp'),
-                            connect().use('/bower_components', connect.static('./bower_components')),
-                            connect.static(config.app)
-                        ];
-                    }
-                }
-            },
-            dist: {
-                options: {
-                    base: '<%= config.dist %>',
-                    livereload: false
-                }
-            }
-        },
-
-        // Empties folders to start fresh
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%= config.dist %>/*',
-                        '!<%= config.dist %>/.git*',
-                        '!<%= config.dist %>/CNAME',
-                        '!<%= config.dist %>/README.md'
-                    ]
-                }]
-            },
-            server: '.tmp'
-        },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfile.js',
-                '<%= config.app %>/scripts/{,*/}*.js',
-                '!<%= config.app %>/scripts/vendor/*'
-            ]
-        },
-
-        // Compiles Sass to CSS and generates necessary files if requested
-        sass: {
-            options: {
-                includePaths: [
-                    'bower_components'
-                ]
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/styles',
-                    src: ['*.scss'],
-                    dest: '.tmp/styles',
-                    ext: '.css'
-                }]
-            },
-            server: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>/styles',
-                    src: ['*.scss'],
-                    dest: '.tmp/styles',
-                    ext: '.css'
-                }]
-            }
-        },
-
-        // Add vendor prefixed styles
-        autoprefixer: {
-            options: {
-                browsers: ['last 1 version']
-            },
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '.tmp/styles/',
-                    src: '{,*/}*.css',
-                    dest: '.tmp/styles/'
-                }]
-            }
-        },
-
-        // Automatically inject Bower components into the HTML file
-        bowerInstall: {
-            app: {
-                src: ['<%= config.app %>/index.html']
-            },
-            sass: {
-                src: ['<%= config.app %>/styles/{,*/}*.{scss,sass}']
-            }
-        },
-
-        // Renames files for browser caching purposes
-        rev: {
-            dist: {
-                files: {
-                    src: [
-                        '<%= config.dist %>/scripts/{,*/}*.js',
-                        '<%= config.dist %>/styles/{,*/}*.css',
-                        '<%= config.dist %>/images/{,*/}*.*',
-                        '<%= config.dist %>/styles/fonts/{,*/}*.*',
-                        '<%= config.dist %>/*.{ico,png}'
-                    ]
-                }
-            }
-        },
-
-        // Reads HTML for usemin blocks to enable smart builds that automatically
-        // concat, minify and revision files. Creates configurations in memory so
-        // additional tasks can operate on them
-        useminPrepare: {
-            options: {
-                dest: '<%= config.dist %>'
-            },
-            html: '<%= config.app %>/index.html'
-        },
-
-        // Performs rewrites based on rev and the useminPrepare configuration
-        usemin: {
-            options: {
-                assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
-            },
-            html: ['<%= config.dist %>/{,*/}*.html'],
-            css: ['<%= config.dist %>/styles/{,*/}*.css']
-        },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    collapseBooleanAttributes: true,
-                    collapseWhitespace: true,
-                    removeAttributeQuotes: true,
-                    removeCommentsFromCDATA: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.dist %>',
-                    src: '{,*/}*.html',
-                    dest: '<%= config.dist %>'
-                }]
-            }
-        },
-
-        // Copies remaining files to places other tasks can use
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= config.app %>',
-                    dest: '<%= config.dist %>',
-                    src: [
-                        '*.{ico,png,txt}',
-                        '.htaccess',
-                        'images/{,*/}*.webp',
-                        '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
-                    ]
-                }]
-            },
-            styles: {
-                expand: true,
-                dot: true,
-                cwd: '<%= config.app %>/styles',
-                dest: '.tmp/styles/',
-                src: '{,*/}*.css'
-            }
-        },
-
-        // Sets up grunt-build-control
-        buildcontrol: {
-            options: {
-                dir: 'dist',
-                commit: true,
-                push: true,
-                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
-            },
-            pages: {
-                options: {
-                    remote: 'git@github.com:AlecRust/find-my-location.git',
-                    branch: 'gh-pages'
-                }
-            }
-        },
-
-        // Run some tasks in parallel to speed up build process
-        concurrent: {
-            server: [
-                'sass:server',
-                'copy:styles'
-            ],
-            dist: [
-                'sass',
-                'copy:styles'
-            ]
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bowerInstall']
+      },
+      js: {
+        files: ['<%= config.app %>/scripts/{,*/}*.js'],
+        tasks: ['jshint'],
+        options: {
+          livereload: true
         }
-    });
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      sass: {
+        files: ['<%= config.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['sass:server', 'autoprefixer']
+      },
+      styles: {
+        files: ['<%= config.app %>/styles/{,*/}*.css'],
+        tasks: ['newer:copy:styles', 'autoprefixer']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= config.app %>/images/{,*/}*'
+        ]
+      }
+    },
 
-
-    grunt.registerTask('serve', function (target) {
-        if (target === 'dist') {
-            return grunt.task.run(['build', 'connect:dist:keepalive']);
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
         }
+      },
+      dist: {
+        options: {
+          base: '<%= config.dist %>',
+          livereload: false
+        }
+      }
+    },
 
-        grunt.task.run([
-            'clean:server',
-            'concurrent:server',
-            'autoprefixer',
-            'connect:livereload',
-            'watch'
-        ]);
-    });
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*',
+            '!<%= config.dist %>/CNAME',
+            '!<%= config.dist %>/README.md'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
 
-    grunt.registerTask('build', [
-        'clean:dist',
-        'useminPrepare',
-        'concurrent:dist',
-        'autoprefixer',
-        'concat',
-        'cssmin',
-        'uglify',
-        'copy:dist',
-        'rev',
-        'usemin',
-        'htmlmin'
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+
+    // Compiles Sass to CSS and generates necessary files if requested
+    sass: {
+      options: {
+        includePaths: [
+          'bower_components'
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['*.scss'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['last 1 version']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Automatically inject Bower components into the HTML file
+    bowerInstall: {
+      app: {
+        src: ['<%= config.app %>/index.html']
+      },
+      sass: {
+        src: ['<%= config.app %>/styles/{,*/}*.{scss,sass}']
+      }
+    },
+
+    // Renames files for browser caching purposes
+    rev: {
+      dist: {
+        files: {
+          src: [
+            '<%= config.dist %>/scripts/{,*/}*.js',
+            '<%= config.dist %>/styles/{,*/}*.css',
+            '<%= config.dist %>/images/{,*/}*.*',
+            '<%= config.dist %>/styles/fonts/{,*/}*.*',
+            '<%= config.dist %>/*.{ico,png}'
+          ]
+        }
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: '<%= config.dist %>'
+      },
+      html: '<%= config.app %>/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
+      },
+      html: ['<%= config.dist %>/{,*/}*.html'],
+      css: ['<%= config.dist %>/styles/{,*/}*.css']
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: '{,*/}*.html',
+          dest: '<%= config.dist %>'
+        }]
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            'images/{,*/}*.webp',
+            '{,*/}*.html',
+            'styles/fonts/{,*/}*.*'
+          ]
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Sets up grunt-build-control
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:AlecRust/find-my-location.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
+
+    // Run some tasks in parallel to speed up build process
+    concurrent: {
+      server: [
+        'sass:server',
+        'copy:styles'
+      ],
+      dist: [
+        'sass',
+        'copy:styles'
+      ]
+    }
+  });
+
+
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
     ]);
+  });
 
-    grunt.registerTask('default', [
-        'newer:jshint',
-        'build'
-    ]);
+  grunt.registerTask('build', [
+    'clean:dist',
+    'useminPrepare',
+    'concurrent:dist',
+    'autoprefixer',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:dist',
+    'rev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'build'
+  ]);
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,130 +10,130 @@ var map,
 
 // Initialise Google Map
 function initialize() {
-    
-    var mapOptions = {
-        backgroundColor: '#005b41',
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    };
-    
-    map = new google.maps.Map(document.getElementById('location-map'), mapOptions);
-    geocoder = new google.maps.Geocoder();
 
-    // Try HTML5 geolocation
-    if (navigator.geolocation) {
-        
-        // Browser supports Geolocation
-        navigator.geolocation.getCurrentPosition(function (position) {
-            
-            // Remove loading message
-            var loadingMessage = document.getElementById('loading-message');
-            loadingMessage.parentNode.removeChild(loadingMessage);
-            
-            // Get lat/lng of user
-            var userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var mapOptions = {
+    backgroundColor: '#005b41',
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
 
-            // Configure info window
-            var infoWindow = new google.maps.InfoWindow({
-                content: '<div class="info-window">' +
-                    '<dl id="info-window-content">' +
-                    '<dt>Lat:</dt><dd>' + userPosition.lat() + '</dd>' +
-                    '<dt>Lng:</dt><dd>' + userPosition.lng() + '</dd>' +
-                    '</dl>' +
-                    '</div>'
-            });
-            
-            // Output lat/lng to console
-            console.log('Latitude: ' + userPosition.lat());
-            console.log('Longitude: ' + userPosition.lng());
+  map = new google.maps.Map(document.getElementById('location-map'), mapOptions);
+  geocoder = new google.maps.Geocoder();
 
-            // Configure map marker
-            var marker = new google.maps.Marker({
-                map: map,
-                position: userPosition,
-                title: 'Click to zoom'
-            });
+  // Try HTML5 geolocation
+  if (navigator.geolocation) {
 
-            // Set map center to user position
-            map.setCenter(userPosition);
+    // Browser supports Geolocation
+    navigator.geolocation.getCurrentPosition(function (position) {
 
-            // Set map hue colour
-            map.set('styles', [{
-                stylers: [
-                    { hue: '#005b41' }
-                ]
-            }]);
+      // Remove loading message
+      var loadingMessage = document.getElementById('loading-message');
+      loadingMessage.parentNode.removeChild(loadingMessage);
 
-            // Open info window on marker
-            infoWindow.open(map, marker);
+      // Get lat/lng of user
+      var userPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-            // Reverse Geocode lat/lng
-            geocoder.geocode({ 'latLng': userPosition }, function (results, status) {
-                var infoWindowContent = document.getElementById('info-window-content');
-                var userAddress;
-                var userAddressFormatted = results[0].formatted_address;
-                if (status === google.maps.GeocoderStatus.OK) {
-                    userAddress = userAddressFormatted;
-                    console.log('Address: ' + userAddressFormatted);
-                } else {
-                    userAddress = '<span class="error">Street address not available</span>';
-                    console.log('Geocode failed: ' + status);
-                }
+      // Configure info window
+      var infoWindow = new google.maps.InfoWindow({
+        content: '<div class="info-window">' +
+                   '<dl id="info-window-content">' +
+                     '<dt>Lat:</dt><dd>' + userPosition.lat() + '</dd>' +
+                     '<dt>Lng:</dt><dd>' + userPosition.lng() + '</dd>' +
+                   '</dl>' +
+                 '</div>'
+      });
 
-                // Append address line to info window
-                infoWindowContent.innerHTML = infoWindowContent.innerHTML + '<dt>Adr:</dt><dd>' + userAddress + '</dd>';
-            });
+      // Output lat/lng to console
+      console.log('Latitude: ' + userPosition.lat());
+      console.log('Longitude: ' + userPosition.lng());
 
-            // Zoom to marker on click
-            google.maps.event.addListener(marker, 'click', function () {
-                map.panTo(userPosition);
-                map.setZoom(19);
-                map.setTilt(45);
-            });
-            
-        }, function () {
-            // Browser supports Geolocation, but user declined
-            handleNoGeolocation(true);
-        });
-        
-    } else {
-        // Browser doesn't support Geolocation
-        handleNoGeolocation(false);
-    }
+      // Configure map marker
+      var marker = new google.maps.Marker({
+        map: map,
+        position: userPosition,
+        title: 'Click to zoom'
+      });
+
+      // Set map center to user position
+      map.setCenter(userPosition);
+
+      // Set map hue colour
+      map.set('styles', [{
+        stylers: [
+          { hue: '#005b41' }
+        ]
+      }]);
+
+      // Open info window on marker
+      infoWindow.open(map, marker);
+
+      // Reverse Geocode lat/lng
+      geocoder.geocode({ 'latLng': userPosition }, function (results, status) {
+        var infoWindowContent = document.getElementById('info-window-content');
+        var userAddress;
+        var userAddressFormatted = results[0].formatted_address;
+        if (status === google.maps.GeocoderStatus.OK) {
+          userAddress = userAddressFormatted;
+          console.log('Address: ' + userAddressFormatted);
+        } else {
+          userAddress = '<span class="error">Street address not available</span>';
+          console.log('Geocode failed: ' + status);
+        }
+
+        // Append address line to info window
+        infoWindowContent.innerHTML = infoWindowContent.innerHTML + '<dt>Adr:</dt><dd>' + userAddress + '</dd>';
+      });
+
+      // Zoom to marker on click
+      google.maps.event.addListener(marker, 'click', function () {
+        map.panTo(userPosition);
+        map.setZoom(19);
+        map.setTilt(45);
+      });
+
+    }, function () {
+      // Browser supports Geolocation, but user declined
+      handleNoGeolocation(true);
+    });
+
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
 }
 
 // Show error message info window
 function handleNoGeolocation(errorFlag) {
-    var errorContent,
-        defaultLatLng = new google.maps.LatLng(52, -1);
+  var errorContent,
+      defaultLatLng = new google.maps.LatLng(52, -1);
 
-    // Remove loading message
-    var loadingMessage = document.getElementById('loading-message');
-    loadingMessage.parentNode.removeChild(loadingMessage);
-    
-    if (errorFlag) {
-        errorContent = 'Please allow location information';
-    } else {
-        errorContent = 'Your browser doesn\'t support Geolocation';
-    }
+  // Remove loading message
+  var loadingMessage = document.getElementById('loading-message');
+  loadingMessage.parentNode.removeChild(loadingMessage);
 
-    var infoWindowOptions = {
-        map: map,
-        position: defaultLatLng,
-        content: '<div class="info-window-error">' +
-            '<p>' + errorContent + '</p>' +
-            '</div>'
-    };
+  if (errorFlag) {
+    errorContent = 'Please allow location information';
+  } else {
+    errorContent = 'Your browser doesn\'t support Geolocation';
+  }
 
-    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-    map.setCenter(infoWindowOptions.position);
+  var infoWindowOptions = {
+    map: map,
+    position: defaultLatLng,
+    content: '<div class="info-window-error">' +
+               '<p>' + errorContent + '</p>' +
+             '</div>'
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+  map.setCenter(infoWindowOptions.position);
 }
 
 // Asynchronously load Google Maps API
 function loadScript() {
-    var script = document.createElement('script');
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' + 'callback=initialize';
-    document.body.appendChild(script);
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&' + 'callback=initialize';
+  document.body.appendChild(script);
 }
 
 window.onload = loadScript;
